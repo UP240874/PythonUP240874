1059044cffe6768e8907480a8af5c76870ea2022
UP240874 <up240874@alumnos.upa.edu.mx>
2025-06-05 10:46:29 -0600

    creacion del archivo 1

---
+++ day1_level2.py
@@ -0,0 +1,27 @@
+#ejercicio 1
+print(3+4) 
+print(3-4) 
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#ejercicio3
+print("andrea")
+print("irma, victor, lexcy, wanda")
+print("mexico")
+print("I am enjoying 30 days of python")
+
+
+#ejercicio3 
+print(type(10))
+print(type(938))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type("andrea"))
+print(type("irma, victor, lexcy, wanda"))
+print(type("mexico"))
+
+
